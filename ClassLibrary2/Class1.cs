2f5012aa5604c0d4c974a70061a20fd23d69dff1
316de <316de@315-DE-9>
2023-07-05 09:36:05 +0300

    Добавьте файлы проекта.

---
+++ ClassLibrary2/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary2
+{
+    //Мартынов В.В.
+    public class Garden
+    {
+        public string Address { get; set; }
+
+        public string Owner{ get; set; }
+
+        public string Flowers{ get; set; }
+        public List<Flower> Lib = new List<Flower>();
+
+        public Garden(string add, string owner, string flowers)
+        {
+            Address = add;
+            Owner = owner;
+            Flowers = flowers;
+        }
+
+        public void ShowAll()
+        {
+            Console.WriteLine("Сад:\n" + $"Адрес: {Address}\n" + $"Владелец: {Owner}\n" + $"Цветок: {Flowers}\n");
+            
+        }
+    }
+    //Мартынов В.В.
+    public class Flower : IComparable
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public Flower(string t, string d)
+        {
+            Title = t;
+            Description = d;
+        }
+        public void Show()
+        {
+            Console.WriteLine("Цветок:\n" + $"Название: {Title}\n" + $"Описание: {Description}");
+        }
+        public int CompareTo(object obj)
+        {
+            return (this.Description + this.Title).CompareTo(Description + Title);
+        }
+        //Мартынов В.В.
+    }
+}
---
+++ Kval/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary2;
+
+namespace Kval
+{
+    internal class Program
+    {
+        //Мартынов В.В.
+        static void Main(string[] args)
+        {
+            Flower fl = new Flower("Ромашка", "Цветок, с белыми лепестакми и желтым кружком в середине");
+            Flower fl2 = new Flower("Одуванчик", "Цветок, который сначала желтый, а потом превращается в пушистый белый комок, на который можно подуть и с него слетят пушинки");
+            Flower fl3 = new Flower("Тюльпан", "Цветок, который чаще всего дарят на 8 марта");
+
+            Garden gar = new Garden("улица Мира, дом 3", "Аня", "Одуванчик");
+            Garden gar2 = new Garden("улица Иванова, дом 5", "Оля", "Ромашка");
+            Garden gar3 = new Garden("улица Ленина, до 54", "Вика", "Тюльпан");
+            
+            fl.Show();
+            fl2.Show();
+            fl3.Show();
+
+            gar.ShowAll();
+            gar2.ShowAll();
+            gar3.ShowAll();
+            
+            
+            
+            Console.ReadKey();
+        }
+        //Мартынов В.В.
+    }
+}
